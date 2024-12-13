f2a4286dd075f76d81f48547bb806866636b39c0
Abhilash Mandula <mandula.abhilash@gmail.com>
2024-12-13 15:07:19 +0530

    Update pills design

--- frontend/components/hero/HeroPill.js
+++ frontend/components/hero/HeroPill.js
@@ -8,12 +8,10 @@ export default function HeroPill({ text, icon: Icon, color, delay }) {
       animate={{ opacity: 1, scale: 1 }}
       transition={{ delay }}
       className={cn(
-        "px-4 py-1 border-2 cursor-default flex items-center gap-2",
-        color === "rose" &&
-          "bg-rose-500/10 text-rose-500 border-rose-500/20 rounded-l-full border-r-0",
-        color === "primary" && "bg-primary/10 text-primary border-primary/20",
-        color === "emerald" &&
-          "bg-emerald-500/10 text-emerald-500 border-emerald-500/20 rounded-r-full border-l-0"
+        "px-4 py-1 cursor-default flex items-center gap-2",
+        color === "rose" && "bg-white text-rose-500 rounded-l-full",
+        color === "primary" && "bg-white text-primary border-x-2",
+        color === "emerald" && "bg-white text-emerald-500 rounded-r-full"
       )}
     >
       {Icon && <Icon className="w-3.5 h-3.5" />}
--- frontend/components/hero/HeroPills.js
+++ frontend/components/hero/HeroPills.js
@@ -3,10 +3,10 @@ import HeroPill from "./HeroPill";
 
 export default function HeroPills() {
   return (
-    <div className="inline-flex text-sm font-semibold shadow-lg rounded-full hover:scale-105 duration-500">
+    <div className="inline-flex text-sm font-semibold shadow-xl rounded-full hover:scale-105 duration-500">
       <HeroPill text="Perfect" icon={Heart} color="rose" delay={0.2} />
-      <HeroPill text="Thoughtful" icon={Sparkles} color="primary" delay={0.3} />
-      <HeroPill text="Memorable" icon={Gift} color="emerald" delay={0.4} />
+      <HeroPill text="Thoughtful" icon={Sparkles} color="primary" delay={0.4} />
+      <HeroPill text="Memorable" icon={Gift} color="emerald" delay={0.6} />
     </div>
   );
 }
